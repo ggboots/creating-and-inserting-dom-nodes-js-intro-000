4ebd995b14df45fa667109b154878dbb36c54edc
George Boots <ggboots.99@gmail.com>
2020-07-08 10:50:00 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,3 +6,9 @@ element.style.backgroundColor = '#F9F9F9';
 
 document.body.appendChild(element);
 element.style.textAlign = 'center';
+
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
